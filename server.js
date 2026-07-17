e63a0902d5aef9c2890e4e11e15b2422af97ef50
Tuan Ha <hatuan@email.cz>
2026-07-17 20:15:40 +0200

    Routing

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  fs.readFile("./index.html", (err, data) => {
+  let pagePath = "./";
+  switch (req.url) {
+    case "/":
+      pagePath += "index.html";
+      break;
+    case "/about":
+      pagePath += "about.html";
+      break;
+    case "/contact-me":
+      pagePath += "contact-me.html";
+      break;
+    default:
+      pagePath += "404.html";
+      break;
+  }
+
+  fs.readFile(pagePath, (err, data) => {
     if (err) {
       console.log(err);
-      return;
+      res.end("not found");
     }
 
     res.end(data);
